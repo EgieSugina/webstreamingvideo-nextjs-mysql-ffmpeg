e31576c09e832adb981715727ae01fc02f6408ba
Egie Sugina <egiesugina704@gmail.com>
2024-02-27 02:58:55 +0000

    add

--- app/home/layout.js
+++ app/home/layout.js
@@ -3,25 +3,25 @@ import Menu from "./menu";
 export default function HomeLayout({ children }) {
   return (
     <>
-      <div class="min-h-full">
-        <nav class="glass">
-          <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div class="flex h-16 items-center justify-between">
-              <div class="flex items-center">
-                <div class="flex-shrink-0">NAMA WEB</div>
-                <div class="hidden md:block">
-                  <div class="ml-10 flex items-baseline space-x-4">
+      <div className="min-h-full">
+        <nav className="glass">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">NAMA WEB</div>
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
                     <Menu name="Home" href="/home" />
                     <Menu name="Movie" href="/movie" />
                   </div>
                 </div>
               </div>
-              <div class="hidden md:block">AAA</div>
+              <div className="hidden md:block">AAA</div>
             </div>
           </div>
         </nav>
         <main>
-          <div class="mx-auto  py-6 sm:px-6 lg:px-8">{children}</div>
+          <div className="mx-auto  py-6 sm:px-6 lg:px-8">{children}</div>
         </main>
       </div>
     </>
